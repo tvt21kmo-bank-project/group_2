c198bf8d8d7a1bd9a5b7ba4d82e7236b055c3d54
Mikko Turpeinen <t0tumi01@students.oamk.fi>
2021-12-14 16:37:03 +0200

    siivousta

--- Bankautomat/mainwindow.h
+++ Bankautomat/mainwindow.h
@@ -17,48 +17,29 @@ public:
     ~MainWindow();
     void UpdateTransactionsList();
     void NumberKeyClicked(char key);
+    void TimerReset();
 
 private slots:
     void on_funbtn8_clicked();
-
     void on_funbtn1_clicked();
-
     void on_funbtn5_clicked();
-
     void on_funbtn2_clicked();
-
     void timerTick();
-
     void on_btn1_clicked();
-
     void on_btn2_clicked();
-
     void on_bnt3_clicked();
-
     void on_btn4_clicked();
-
     void on_btn5_clicked();
-
     void on_btn6_clicked();
-
     void on_btn7_clicked();
-
     void on_btn8_clicked();
-
     void on_btn9_clicked();
-
     void on_btnhast_clicked();
-
     void on_btn0_clicked();
-
-    void on_btnC_clicked();
-    
+    void on_btnC_clicked();   
     void on_funbtn3_clicked();
-
     void on_funbtn4_clicked();
-
     void on_funbtn6_clicked();
-
     void on_funbtn7_clicked();
 
 private:
